a4692e18998ae326cfa6088a721628557309c061
davidmizra17 <david.mizrahi@correo.unimet.edu.ve>
2023-02-05 15:01:22 -0400

    hero section

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const eMail = document.getElementById("email");
 const rMessage = document.getElementById("rmessage");
 const submitButton = document.getElementById("submit");
 const country = document.getElementById("country");
+const carouselButtons = document.querySelectorAll("[data-carousel-button]")
 
 function printData(){
 
@@ -18,6 +19,23 @@ function printData(){
 submitButton.addEventListener("click", async () => {
     printData();
 })
+
+carouselButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      const offset = button.dataset.carouselButton === "next" ? 1 : -1
+      const slides = button
+        .closest("[data-carousel]")
+        .querySelector("[data-slides]")
+  
+      const activeSlide = slides.querySelector("[data-active]")
+      let newIndex = [...slides.children].indexOf(activeSlide) + offset
+      if (newIndex < 0) newIndex = slides.children.length - 1
+      if (newIndex >= slides.children.length) newIndex = 0
+  
+      slides.children[newIndex].dataset.active = true
+      delete activeSlide.dataset.active
+    })
+  })
 
 
 
