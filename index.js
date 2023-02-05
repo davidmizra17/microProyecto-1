42cc275187d4bb7488c85e8fdce52a61b1b4b0ff
davidmizra17 <david.mizrahi@correo.unimet.edu.ve>
2023-02-05 12:48:15 -0400

    contact section advance

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-const variable = document.getElementById("");
+const firstName = document.getElementById("fname");
+const lastName = document.getElementById("lname");
+const eMail = document.getElementById("email");
+const rMessage = document.getElementById("rmessage");
+const submitButton = document.getElementById("submit");
+const country = document.getElementById("country");
 
-console.log("HOLA PUTA");
+function printData(){
+
+    alert("RECRUITER INFO:\n" + "Name: " + firstName.value + "\n" + 
+    "Last Name: " + lastName.value + "\n" +
+    "E mail: " + eMail.value + "\n" + 
+    "Based in: " + country.value);
+    alert("Your request has been sent!")
+}
+
+
+submitButton.addEventListener("click", async () => {
+    printData();
+})
+
+
+
+
+
+
+
+
